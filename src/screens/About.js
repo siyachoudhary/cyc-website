b2871af376bd36fecc1a21dbc0495fc8ad7c10a6
Siya Choudhary <Siyacutesiya@gmail.com>
2023-06-12 11:21:52 -0500

    CYC helping mobile responsive

--- src/screens/About.js
+++ src/screens/About.js
@@ -27,16 +27,16 @@ export const About = () => {
         </div>
         <h2 className='headingBlue'>WHY IS OUR MISSION SO IMPORTANT?</h2>
         {/* data graphics */}
-        <p className='aboutInfoSmall'>So much of the world around us is run by technology. If women are not contributing to these systems and products, bias becomes imbedded in the algorithms, data sets and designs that influence our future.</p>
+        <p className='aboutInfoSmall' data-AOS="fade-up">So much of the world around us is run by technology. If women are not contributing to these systems and products, bias becomes imbedded in the algorithms, data sets and designs that influence our future.</p>
         <div className='aboutInfo NYTimes' data-AOS="fade-up">
           <p className='NYTimesData'>"Many artificial intelligence technologies, like face-recognition services and conversational systems, are designed to learn from large amounts of data, such as thousands of photos of faces. The biases of researchers can easily be introduced into the technology, reinforcing the importance of diversity among the people working on it."</p>
           <p className='NYTimesSign'>~ Cade Metz, The New York Times</p>
         </div>
 
         <h2 className='headingOrange'>WHAT CYC IS DOING TO HELP</h2>
-        <div className='aboutInfo' data-AOS="fade-up">
-          <p className='cycHelpText'>CYC generates enthusiasm for computer science through interactive workshops that focus on the many creative uses of code. Girls learn firsthand how computer science is used in movies, video games, virtual reality, augmented reality and more. Our hands-on projects aim to build confidence and show our students that anyone can learn to code!</p>
-          <div className='workshopStats'>
+        <div className='aboutInfo'>
+          <p className='cycHelpText' data-AOS="fade-up">CYC generates enthusiasm for computer science through interactive workshops that focus on the many creative uses of code. Girls learn firsthand how computer science is used in movies, video games, virtual reality, augmented reality and more. Our hands-on projects aim to build confidence and show our students that anyone can learn to code!</p>
+          <div className='workshopStats' data-AOS="fade-up">
             On average, girls show a 72.3% improvement on our knowledge survey after a workshop!
           </div>
         </div>
